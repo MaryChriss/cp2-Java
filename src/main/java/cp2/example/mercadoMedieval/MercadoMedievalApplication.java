9d6df677f3be6cdfe9c416e09ce81431c750fb31
MaryChriss <marychris.crf@gmail.com>
2025-04-22 19:17:39 -0300

    add: pom dependecies

---
+++ src/main/java/cp2/example/mercadoMedieval/MercadoMedievalApplication.java
@@ -0,0 +1,13 @@
+package cp2.example.mercadoMedieval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadoMedievalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadoMedievalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cp2/example/mercadoMedieval/MercadoMedievalApplicationTests.java
@@ -0,0 +1,13 @@
+package cp2.example.mercadoMedieval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoMedievalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
